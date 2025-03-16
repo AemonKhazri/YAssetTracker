e71d0d136d00142830d4a6dec7c7216f4dd43b5e
Aemon33 <khazriaymen93@gmail.com>
2025-03-16 23:52:35 +0100

    add mongoDB Asset data access method

---
+++ YAssetsTrackerLibrary/DataAccess/IAssetData.cs
@@ -0,0 +1,8 @@
+﻿
+namespace YAssetsTrackerLibrary.DataAccess;
+
+public interface IAssetData
+{
+   Task CreateAsset(AssetModel asset);
+   Task<List<AssetModel>> GetAllAssets();
+}
---
+++ YAssetsTrackerLibrary/DataAccess/MongoAssetData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YAssetsTrackerLibrary.DataAccess
+{
+   public class MongoAssetData : IAssetData
+   {
+      private readonly IMongoCollection<AssetModel> _assets;
+
+      public MongoAssetData(IDbConnection db)
+      {
+         _assets = db.AssetCollection;
+
+      }
+      public async Task<List<AssetModel>> GetAllAssets()
+      {
+         var results = await _assets.FindAsync(_ => true);
+         return results.ToList();
+      }
+      public Task CreateAsset(AssetModel asset)
+      {
+         return _assets.InsertOneAsync(asset);
+      }
+
+   }
+}
